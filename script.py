646af432de31ac8ffd8eef81a3e6ee39cff2bd44
Suhovienko Ilia <isuhovienko@gmail.com>
2020-08-16 05:31:59 +0300

    Updates after course chapter

--- script.py
+++ script.py
@@ -1,3 +1,7 @@
-print("X O X")
-print("O X O")
-print("X X O")
+cells = input('Enter cells: > ')
+
+print('---------')
+print("| " + cells[0] + " " + cells[1] + " " + cells[2] + " | ")
+print("| " + cells[3] + " " + cells[4] + " " + cells[5] + " | ")
+print("| " + cells[6] + " " + cells[7] + " " + cells[-1] + " | ")
+print('---------')
